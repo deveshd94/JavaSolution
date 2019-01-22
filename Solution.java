888b090e3d2cc8d056ac10bfa2a2e3d1b35dcac5
deveshd94 <deveshd94@gmail.com>
2019-01-22 20:08:11 +0530

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,175 +1,113 @@
-/*package whatever //do not write package name here */
-
 import java.util.*;
-import java.lang.*;
-import java.io.*;
 
-class Point
+
+class Graph
 {
-    int X;
-    int Y;
-    public Point(int X,int Y)
-    {
-        this.X = X;
-        this.Y = Y;
-    }
-    
-    public Point(Point y)
-    {
-    	this.X = y.X;
-    	this.Y = y.Y;
-    }
-    
-    public Stack<String> getPath(Point dst,int m,int n)
-    {
-    	if(dst.X>m)
-    		return null;
-        Queue<Point> res = new LinkedList<Point>();
-        Stack<String> path = new Stack<String>();
-        HashMap<String,String> parentHash = new HashMap<String,String>();
-        HashMap<String,Integer> myHash = new HashMap<String,Integer>();
-        res.add(this);
-        parentHash.put("0,0", null);
-        Point myPoint = new Point(0,0);
-        Point tmp = new Point(0,0);
-        int diff=0;
-        while(!res.isEmpty() && (dst.X!=tmp.X || dst.Y!=tmp.Y))
-        {
-            tmp = res.remove();
-            //System.out.println("X : "+tmp.X+"Y : "+tmp.Y);
-            myHash.put(tmp.X+","+tmp.Y,1);
-            //Case 1 : Empty a Jug
-            myPoint.X = 0;
-            myPoint.Y = tmp.Y;
-            if(!(myHash.containsKey(myPoint.X+","+myPoint.Y)))
-            {
-            	parentHash.put(myPoint.X+","+myPoint.Y, tmp.X+","+tmp.Y);
-                res.add(new Point(myPoint));
-            }
-            myPoint.X = tmp.X;
-            myPoint.Y = 0;
-            if(!(myHash.containsKey(myPoint.X+","+myPoint.Y)))
-            {
-            	parentHash.put(myPoint.X+","+myPoint.Y, tmp.X+","+tmp.Y);
-                res.add(new Point(myPoint));
-            }
-            //Case 2 : Fill a Jug
-            myPoint.X = tmp.X;
-            myPoint.Y = n;
-            if(!(myHash.containsKey(myPoint.X+","+myPoint.Y)))
-            {
-            	parentHash.put(myPoint.X+","+myPoint.Y, tmp.X+","+tmp.Y);
-                res.add(new Point(myPoint));
-            }
-            myPoint.X = m;
-            myPoint.Y = tmp.Y;
-            if(!(myHash.containsKey(myPoint.X+","+myPoint.Y)))
-            {
-            	parentHash.put(myPoint.X+","+myPoint.Y, tmp.X+","+tmp.Y);
-                res.add(new Point(myPoint));
-            }
-            //Case 3 :Transfer
-            if((tmp.X+tmp.Y)>=m)
-            {
-                diff = m-tmp.X;
-                myPoint.X = m;
-                myPoint.Y = tmp.Y-diff;
-                if(!(myHash.containsKey(myPoint.X+","+myPoint.Y)))
-                {
-                	parentHash.put(myPoint.X+","+myPoint.Y, tmp.X+","+tmp.Y);
-                    res.add(new Point(myPoint));
-                }
-                if((tmp.X+tmp.Y)>=n)
-                {
-                    diff = n-tmp.Y;
-                    myPoint.X = tmp.X-diff;
-                    myPoint.Y = n;
-                    if(!(myHash.containsKey(myPoint.X+","+myPoint.Y)))
-                    {
-                    	parentHash.put(myPoint.X+","+myPoint.Y, tmp.X+","+tmp.Y);
-                        res.add(new Point(myPoint));
-                    }
-                }
-            }
-            if((tmp.X+tmp.Y)<m)
-            {  
-                myPoint.X = tmp.X+tmp.Y;//please check from here: Inputs are : 3,4,2
-                myPoint.Y=0;
-                if(!(myHash.containsKey(myPoint.X+","+myPoint.Y)))
-                {
-                	parentHash.put(myPoint.X+","+myPoint.Y, tmp.X+","+tmp.Y);
-                    res.add(new Point(myPoint));
-                }
-            }
-            if((tmp.X+tmp.Y)<n)
-            {
-                myPoint.X = 0;
-                myPoint.Y = tmp.X+tmp.Y;
-                if(!(myHash.containsKey(myPoint.X+","+myPoint.Y)))
-                {
-                	parentHash.put(myPoint.X+","+myPoint.Y, tmp.X+","+tmp.Y);
-                    res.add(new Point(myPoint));
-                }    
-            }
-            
-    }
-      if(res.isEmpty() && (dst.X!=tmp.X || dst.Y!=tmp.Y))
-      {
-    	  System.out.print(-1);
-    	  return null;
-      }
-        
-      String myRes=tmp.X+","+tmp.Y;
-      while(myRes!=null)
-      {
-    	  //Extract the parent of tmp.
-    	  path.push(myRes);
-    	  myRes = parentHash.get(myRes);
-    	  
-      }
-      
-        return path;
-}
-    
-    
-    public Stack<String> getPath(int dst,int m,int n)
-    {
-        Point one = new Point(dst,0);
-        Point two = new Point(0,dst);
-        Stack<String> myStack1 = getPath(one,m,n);
-        Stack<String> myStack2 = getPath(two,m,n);
-        if(myStack2==null)
-            return null;
-        if(myStack1==null)
-        {
-            return myStack2;
-        }
-        if(myStack1.size()>myStack2.size())
-        {
-            return myStack2;
-        }
-        return myStack1;
-    }
+	int V;
+	int[][] weightMat;
+	public Graph(int Vertex)
+	{
+		V=Vertex;
+		weightMat = new int[V][V];
+	}
+	public void AddEdge(int src,int dst,int weight)
+	{
+		weightMat[src][dst] = weight;
+		weightMat[dst][src] = weight;
+	}
+	
+	public static int getMin(int[] arr)
+	{
+		int min = Integer.MAX_VALUE;
+		int index =0;
+		for(int i=0;i<arr.length;i++)
+		{
+			if(arr[i]<min)
+			{
+				min = arr[i];
+				index = i;
+			}
+		}
+		return index;
+	}
+	
+	public int[][] PrimMST()
+	{
+		boolean[] visited = new boolean[V];
+		int[] arr = new int[V];
+		int[] parent = new int[V];
+		for(int i=0;i<V;i++)
+		{
+			arr[i] = Integer.MAX_VALUE;
+			parent[i] = Integer.MAX_VALUE;
+		}
+		arr[0]=0;//My Source Vertex;
+		int count=V;
+		int[][] res = new int[V][V];//Include the results.
+		parent[0]=-1;
+		while(count>0)
+		{
+			count--;
+			//Fetch Index of Minimum Vertex;
+			int minIndex = getMin(arr);
+			//Iterate through the Index and allot the values int array arr[] & put the minIndex in parent[].
+			for(int i=0;i<weightMat[minIndex].length;i++)
+			{
+				int p = weightMat[minIndex][i];
+				if(p!=0 && !visited[i])
+				{
+					//Change the Adjacent Matrix Values & Parent[].
+					if(arr[i]>p)
+					{
+						//Update the Values.
+						arr[i]=p;
+						parent[i] = minIndex;
+						
+					}
+				}
+			}
+			//Add the details in the res Matrix;
+			if(parent[minIndex]!=-1)
+			{
+				res[minIndex][parent[minIndex]]=arr[minIndex];
+				res[parent[minIndex]][minIndex]=arr[minIndex];
+			}
+			visited[minIndex]=true;
+			//Update the minIndex.
+			arr[minIndex] = Integer.MAX_VALUE;
+		}
+	return res;	
+	}
 }
 
 
-public class Solution {
-	public static void main (String[] args) {
-            Scanner myScanner = new Scanner(System.in);
-            int queries = myScanner.nextInt();
-            while(queries>0)
-            {
-                queries--;
-                int m = myScanner.nextInt();
-                int n = myScanner.nextInt();
-                int d = myScanner.nextInt();
-                Point initialPoint = new Point(0,0);
-                Stack<String> res = initialPoint.getPath(d,m,n);
-                while(!res.isEmpty())
-                {
-                	System.out.println(res.pop());
-                }
-            }
-            myScanner.close();
-	}        
+public class Solution
+{
+	public static void main(String[] args)
+	{
+		Graph myGraph = new Graph(9);
+		myGraph.AddEdge(0,1,4);
+		myGraph.AddEdge(0,7,8);
+		myGraph.AddEdge(1,2,8);
+		myGraph.AddEdge(1,7,11);
+		myGraph.AddEdge(2,3,7);
+		myGraph.AddEdge(2,5,4);
+		myGraph.AddEdge(2,8,2);
+		myGraph.AddEdge(3,5,14);
+		myGraph.AddEdge(3,4,9);
+		myGraph.AddEdge(4,5,10);
+		myGraph.AddEdge(5,6,2);
+		myGraph.AddEdge(6,7,1);
+		myGraph.AddEdge(6,8,6);
+		myGraph.AddEdge(7,8,7);
+		int[][] res = myGraph.PrimMST();
+		for(int i=0;i<res.length;i++)
+		{
+			for(int j=0;j<res.length;j++)
+			{
+				System.out.print(res[i][j]+",");
+			}
+			System.out.println();
+		}
+		}
 }
